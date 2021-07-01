05a90aeac012b44b36542288192b8caa3fd72221
JoshuaLingbloom <Lingbloomlmp@gmail.com>
2021-07-01 16:30:26 -0700

    commit tree 521b6998aec86fa76e02bb19c928448fd20e0ccd

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+let drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const returnFirstTwoDrivers = (drivers) => {
+    let firstDrivers = drivers.slice(0,2)
+    return firstDrivers
+}
+const returnLastTwoDrivers = (drivers) => {
+    let firstDrivers = drivers.slice(-2)
+    return firstDrivers
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (price) => {
+    return total => total = (Math.pow(price, 2))
+}
+
+const fareDoubler = (price) => {
+    return total => total = (price * 2)
+}
+
+const fareTripler = (price) => {
+    return total => total = (price *3)
+}
